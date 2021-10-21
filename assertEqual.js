98f100f761dc8758520ef7f7a512be9924d98ec3
Abby <abby_faigao@yahoo.com>
2021-10-21 23:44:25 +0000

    made an assertion function

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected} `);
+  }
+};
+
