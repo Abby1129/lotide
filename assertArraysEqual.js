6b58664f61de58aac2bc54ffa058a459c07d7560
Abby <abby_faigao@yahoo.com>
2021-10-23 22:30:30 +0000

    function that takes in 2 arrays and returns true if they are equal and false otherwise

---
+++ assertArraysEqual.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1 === array2)) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${array1} !== ${array2} `);
+  }
+};
