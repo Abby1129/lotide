e87338ea650a9b3c908fdfea3f3c45c11e72427a
Abby <abby_faigao@yahoo.com>
2021-10-25 20:06:58 +0000

    updated eqArray

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,4 @@
 const eqArrays = function(array1, array2){
-  if(array1.length !== array2.length){
-    return false
-  }
   for (let i = 0; i < array1.length; i++){
     if (array1[i] !== array2[i] || typeof array1[i] !== typeof array2[i]){
       return false
@@ -9,3 +6,4 @@ const eqArrays = function(array1, array2){
   }
   return true
 }
+
