3fdc446759411b4f027456035ee1d5c14bfe7c64
Abby <abby_faigao@yahoo.com>
2021-10-25 20:08:03 +0000

    updated assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,7 @@
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1 === array2)) {
-    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+const assertArraysEqual = function (array, expected) {
+  if (eqArrays(array === expected)) {
+    console.log(`✅ Assertion Passed: ${array} === ${expected}`);
   } else {
-    console.log(`🔴 Assertion Failed: ${array1} !== ${array2} `);
+    console.log(`🔴 Assertion Failed: ${array} !== ${expected} `);
   }
 };
