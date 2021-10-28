dec0ca21132f27ac4fabbb189743d1b71c7159f6
Abby <abby_faigao@yahoo.com>
2021-10-28 20:26:33 +0000

    function that takes in 2 objects and returns true if they are equal and false otherwise

---
+++ assertObjectsEqual.js
@@ -0,0 +1,13 @@
+// requires eqObjects.js func
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require("util").inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(
+      `✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
+    );
+  } else {
+    console.log(
+      `🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)} `
+    );
+  }
+};
