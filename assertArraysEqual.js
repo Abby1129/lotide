e7d5f8296ca620be40f734f81d3e5b26a75252fe
Abby <abby_faigao@yahoo.com>
2021-11-10 21:08:57 +0000

    refactored and modularized

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,13 @@
-const assertArraysEqual = function (array, expected) {
+const eqArrays = require('../lotide/eqArrays');
+
+const assertArraysEqual = function(array, expected) {
   if (eqArrays(array === expected)) {
     console.log(`✅ Assertion Passed: ${array} === ${expected}`);
   } else {
     console.log(`🔴 Assertion Failed: ${array} !== ${expected} `);
   }
 };
+
+module.exports = assertArraysEqual;
+
+
