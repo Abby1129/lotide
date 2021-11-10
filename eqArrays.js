1fe7c7c6ab3074d223ea5489e18f52e93be68f3e
Abby <abby_faigao@yahoo.com>
2021-11-10 21:03:50 +0000

    refactored and modularized

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,10 @@
-const eqArrays = function(array1, array2){
-  for (let i = 0; i < array1.length; i++){
-    if (array1[i] !== array2[i] || typeof array1[i] !== typeof array2[i]){
-      return false
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i] || typeof array1[i] !== typeof array2[i]) {
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
+module.exports = eqArrays;
