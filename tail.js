26038d9d44be6947985e7751bfb3e8ebd60e9c11
Abby <abby_faigao@yahoo.com>
2021-11-10 20:58:34 +0000

    refactored and modularized)

--- tail.js
+++ tail.js
@@ -1,3 +1,10 @@
-const tail = function (array) {
+const tail = function(array) {
+  if (array.length === 0) {
+    return undefined;
+  } else if (array.length === 1) {
+    return [];
+  }
   return array.slice(1);
 };
+
+module.exports = tail;
